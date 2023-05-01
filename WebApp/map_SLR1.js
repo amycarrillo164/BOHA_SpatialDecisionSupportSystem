f6cc438a3555f7a1e44c997fcd108ee1e8061bb3
amycarrillo164 <amy.carrillo22@myhunter.cuny.edu>
2023-05-01 00:09:04 -0400

    style changes

--- WebApp/map_SLR1.js
+++ WebApp/map_SLR1.js
@@ -1,10 +1,4 @@
 
-// // Layers
-// var SLR1_10pct = L.geoJSON(SLR1_10, {style: style_feature_SLR10,});
-// var SLR1_1pct = L.geoJSON(SLR1_1, {style: style_feature_SLR1,});
-
-
-
 // Set up initial map
 var map = L.map('map', {
     center: [42.3131628, -70.9297749], 
@@ -18,12 +12,6 @@ var map = L.map('map', {
     // },
 });
 
-
-/* Control panel to display map layers */
-// var controlLayers = L.control.layers( null, null, {
-//    position: "topright",
-//    collapsed: false
-// }).addTo(map);
 
 // display Carto basemap tiles with light features and labels
 var light = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
@@ -47,47 +35,6 @@ var baseMaps = {
     "Stamen Terrain": terrain, 
 
 };
-
-
-//var layerControl = L.control.layers(baseMaps).addTo(map);
-
-
-
-// var lC = L.control
-//   .layers(baseMaps, overlayMaps, {
-//     collapsed: false,
-//     hideSingleBase: true,
-//   })
-//   .addTo(map)
-//   .expand();
-
-//POINTS (CVS)
-
-// Read markers data from data.csv
-$.get('geojson_files/cultural_points.csv', function(csvString) {
-
-// Use PapaParse to convert string to array of objects
-var data = Papa.parse(csvString, {header: true, dynamicTyping: true}).data;
-
-// For each row in data, create a marker and add it to the map
-// For each row, columns `Latitude`, `Longitude`, and `Title` are required
-for (var i in data) {
-  var row = data[i];
-
-  var marker = L.marker([row.Latitude, row.Longitude], {
-    opacity: 1,
-  
-  //customize your icon
-  icon: L.icon({
-    iconUrl: "https://img.icons8.com/plasticine/100/null/map-pin.png",
-    iconSize: [12, 10]
-  })
-  }).bindPopup(row.Island);
-     
-  marker.addTo(map);
-  }
-});
-
 
 function onEachFeatureFn(feature, layer) {
   var popupContent =
@@ -120,6 +67,38 @@ function onEachFeatureFn(feature, layer) {
   });
 }
 
+
+//POINTS (CVS)
+
+// Read markers data from data.csv
+$.get('geojson_files/cultural_points.csv', function(csvString) {
+// Use PapaParse to convert string to array of objects
+var data = Papa.parse(csvString, {header: true, dynamicTyping: true}).data;
+// For each row in data, create a marker and add it to the map
+// For each row, columns `Latitude`, `Longitude`, and `Title` are required
+for (var i in data) {
+  var row = data[i];
+  var marker = L.circleMarker([row.Latitude, row.Longitude], {
+    radius: 3,
+    fillColor: 'purple',
+    color: 'purple',
+    weight: 0.1,
+    opacity: 1,
+    fillOpacity: 0.5,
+    pane: 'markerPane',
+    // }).bindTooltip(feature.properties.NAME)
+
+  //customize your icon
+  // icon: L.icon({
+  //   iconUrl: "https://img.icons8.com/plasticine/100/null/map-pin.png",
+  //   iconSize: [12, 10]
+  // })
+  }).bindPopup(row.Island);    
+  marker.addTo(map);
+  }
+});
+
+
 // Style for SLR 2030 1% AEP
 var SLR2030_1 = L.geoJSON(null, {
   style: (feature) => {
@@ -133,10 +112,10 @@ var SLR2030_1 = L.geoJSON(null, {
       //color: "#000", fill: "#ccc", fillOpacity: 0.2 
     };
   },
-  onEachFeature: onEachFeatureFn,
+  //onEachFeature: onEachFeatureFn,
 }).addTo(map);
 
-// Style for SLR 2030 1% AEP
+// Style for SLR 2030 10% AEP
 var SLR2030_10 = L.geoJSON(null, {
   style: (feature) => {
     return { 
@@ -151,21 +130,35 @@ var SLR2030_10 = L.geoJSON(null, {
   onEachFeature: onEachFeatureFn,
 }).addTo(map);
 
+// Style for boundary
+var Boundary = L.geoJSON(null, {
+  style: (feature) => {
+    return { 
+      color: "#ffffff",
+      weight: 1,
+    };
+  },
+}).addTo(map);
+
 //Layers
 var SLR2030_10_CFEP = L.layerGroup([SLR2030_10]);
 var SLR2030_1_CFEP = L.layerGroup([SLR2030_1]);
+var Boundary_9Islands = L.layerGroup([Boundary])
+//var Culture_points = L.layerGroup([])
 
 //Layer groups
 var overlayMaps = {
    //"cultural points" : data,
     "SLR 9in + 10% AEP" : SLR2030_10_CFEP,
     "SLR 9in + 1% AEP" : SLR2030_1_CFEP,
+    "9 Islands Boundary" : Boundary_9Islands,
 };
 
 //Layer box for all layer groups
 var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
 
 
+// FETCHING DATA
 // Fetch geojson file for SLR 2030 1% AEP
 fetch("./geojson_files/c2030_1CFEPpoly.json")
 .then((response) => {
@@ -185,3 +178,81 @@ fetch("./geojson_files/c2030_1CFEPpoly.json")
     console.log(data);
     SLR2030_10.addData(data);
     });
+
+// Fetch BOHA Boundary
+fetch("./geojson_files/BOHA_Boundary.geojson")
+.then((response) => {
+  return response.json();
+})
+.then((data) => {
+  console.log(data);
+  Boundary.addData(data);
+  });
+
+
+// NEED TO WORK ON
+    // Search bar
+var searchControl = new L.Control.Search({
+  layer: Boundary,
+  propertyName: 'Island',
+  //position: 'topright',
+  marker: false,
+  moveToLocation: function (latlng, title, map) {
+      //map.fitBounds( latlng.layer.getBounds() );
+      let zoom = map.getBoundsZoom(latlng.layer.getBounds());
+      map.setView(latlng, zoom); // access the zoom
+  }
+});
+
+searchControl.on('search:locationfound', function (e) {
+
+  //console.log('search:locationfound', );
+
+  //map.removeLayer(this._markerSearch)
+  e.layer.setStyle({ fillColor: '#3f0', color: '#0f0' });
+  if (e.layer._popup)
+      e.layer.openPopup();
+
+}).on('search:collapsed', function (e) {
+
+  featuresLayer.eachLayer(function (layer) {	//restore feature color
+      featuresLayer.resetStyle(layer);
+  });
+});
+
+map.addControl(searchControl);  //inizialize search control
+
+// map.addControl(new L.Control.Search({
+//   url: 'https://nominatim.openstreetmap.org/search?format=json&q={s}&countrycodes=us',
+//   position: 'topleft',
+//   jsonpParam: 'json_callback',
+//   propertyName: 'display_name',
+//   propertyLoc: ['lat', 'lon'],
+//   marker: L.circleMarker([0, 0], { radius: 30 }),
+//   autoCollapse: true,
+//   autoType: false,
+//   minLength: 2,
+//   container: '',
+//   moveToLocation: function (latlng, title, map) {
+//       //map.fitBounds( latlng.layer.getBounds() );
+//       let zoom = 12; //map.getBoundsZoom(latlng.layer.getBounds());
+//       map.setView(latlng, zoom); // access the zoom
+//   }
+// }));
+
+// //search again...
+// var searchLayer = L.layerGroup(Boundary).addTo(map);
+// //... adding data in searchLayer ...
+// map.addControl( new L.Control.Search({layer: Boundary}) );
+// //searchLayer is a L.LayerGroup contains searched markers
+
+// var searchControl = new L.Control.Search({
+//   layer: featuresLayer,
+//   propertyName: 'name',
+//   marker: false,
+//   moveToLocation: function(latlng, title, map) {
+//     //map.fitBounds( latlng.layer.getBounds() );
+//     var zoom = map.getBoundsZoom(latlng.layer.getBounds());
+//       map.setView(latlng, zoom); // access the zoom
+//   }
+// });
