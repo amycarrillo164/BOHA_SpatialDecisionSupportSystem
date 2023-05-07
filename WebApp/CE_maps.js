54288c939811f3001951203e77912aa18de9ad9b
amycarrillo164 <amy.carrillo22@myhunter.cuny.edu>
2023-05-07 01:45:00 -0400

    new markers and nearest point

--- WebApp/CE_maps.js
+++ WebApp/CE_maps.js
@@ -36,20 +36,29 @@ function onEachFeatureFn(feature, layer) {
   });
 }
 
-
+// <!-- script for circle markers -->
+var geojsonMarkerOptions = {
+  radius: 3,
+  fillColor: "#blue",
+  color: "#blue",
+  weight: 1,
+  opacity: .7,
+  fillOpacity: 0.4,
+};
 
 // Set up initial map
 var map = L.map('map', {
+    //fullscreenControl: true,
     center: [42.3131628, -70.9297749], 
     zoom: 13,
     // maxZoom: 20,
     // zoomDelta: 1,
     // zoomControl: true,
-    fullscreenControl: true,
-    // fullscreenControlOptions: {
-    // position: "topleft",
-    // },
+    //fullscreenControlOptions: {
+      //position: 'bottomright'
+    //},
 });
+
 
 // display Carto basemap tiles with light features and labels
 var light = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
@@ -75,6 +84,34 @@ var baseMaps = {
 
 };
 
+// //Points (CVS)
+// // Read markers data from data.csv
+// $.get('geojson_files/cultural_points.csv', function(csvString) {
+//   // Use PapaParse to convert string to array of objects
+//   var data = Papa.parse(csvString, {header: true, dynamicTyping: true}).data;
+//   // For each row in data, create a marker and add it to the map
+//   // For each row, columns `Latitude`, `Longitude`, and `Title` are required
+//   for (var i in data) {
+//     var row = data[i];
+//     var marker = L.circleMarker([row.Latitude, row.Longitude], {
+//       radius: 3,
+//       fillColor: '#0d6efd',
+//       color: '#0d6efd',
+//       weight: 0.1,
+//       opacity: 1,
+//       fillOpacity: 0.5,
+//       pane: 'markerPane',
+//       // }).bindTooltip(feature.properties.NAME)
+  
+//     //customize your icon
+//     // icon: L.icon({
+//     //   iconUrl: "https://img.icons8.com/plasticine/100/null/map-pin.png",
+//     //   iconSize: [12, 10]
+//     // })
+//     }).bindPopup(row.Category);    
+//     marker.addTo(map);
+//     }
+//   });
 
 // Polygons (Geojson)
 // Creating variable & style for Coastal Exposure 2030
@@ -131,7 +168,7 @@ var CE_2030_layer = L.layerGroup([CE_2030]);
 var CE_2050_layer = L.layerGroup([CE_2050]);
 var CE_2070_layer = L.layerGroup([CE_2070]);
 var Boundary_9Islands = L.layerGroup([Boundary]);
-//var marker_points = L.layerGroup([marker])
+//var marker_points = L.layerGroup([all_points])
 var Boundary_search = L.layerGroup([Boundary]);
 
 //Layer groups
@@ -141,7 +178,7 @@ var overlayMaps = {
     "Coastal Exposure 2050" : CE_2050_layer,
     "Coastal Exposure 2070" : CE_2070_layer,
     "9 Islands Boundary" : Boundary_9Islands,
-    //"Cultural & Infrastructure/Facilities" : marker_points,
+    //"Cultural & Infrastructure/Facilities" : focalresource_points,
 };
 
 //Layer box for all layer groups
@@ -188,37 +225,6 @@ legend.addTo(map);
 //file:///C:/Users/amyca/OneDrive/Documents/GTECH732_AdvGIS/Final_Project/BOHA_SpatialDecisionSupportSystem/WebApp/geojson_files/BOHA_Boundary.geojson. 
 //(Reason: CORS request not http).
 
-// NOTE: Had to use js file data method for the data to appear on a public website
-
-
-//Points (CVS)
-// Read markers data from data.csv
-$.get('geojson_files/cultural_points.csv', function(csvString) {
-  // Use PapaParse to convert string to array of objects
-  var data = Papa.parse(csvString, {header: true, dynamicTyping: true}).data;
-  // For each row in data, create a marker and add it to the map
-  // For each row, columns `Latitude`, `Longitude`, and `Title` are required
-  for (var i in data) {
-    var row = data[i];
-    var marker = L.circleMarker([row.Latitude, row.Longitude], {
-      radius: 3,
-      fillColor: '#0d6efd',
-      color: '#0d6efd',
-      weight: 0.1,
-      opacity: 1,
-      fillOpacity: 0.5,
-      pane: 'markerPane',
-      // }).bindTooltip(feature.properties.NAME)
-  
-    //customize your icon
-    // icon: L.icon({
-    //   iconUrl: "https://img.icons8.com/plasticine/100/null/map-pin.png",
-    //   iconSize: [12, 10]
-    // })
-    }).bindPopup(row.Category);    
-    marker.addTo(map);
-    }
-  });
 
 // Fetch geojson file for CE 2030
 fetch("./geojson_files/SLR1_MCE_RiskOutputs_poly.geojson")
@@ -349,79 +355,6 @@ fetch("public/BOHA_Boundary.geojson")
   });
 
 
+//risk layer again
 
-// STOP
-
-
-// Local Search - NEED TO WORK ON
-    //Search bar
-
-//... adding data in searchLayer ...
-//map.addControl(new L.Control.Search({layer: Boundary_9Islands}));
-//searchLayer is a L.LayerGroup contains searched markers
-
-var searchControl = new L.Control.Search({
-  layer: Boundary,
-  propertyName: 'Island',
-  position: 'topleft',
-  marker: false,
-  moveToLocation: function (latlng, map) {
-      map.fitBounds( latlng.layer.getBounds() );
-      let zoom = map.getBoundsZoom(latlng.layer.getBounds());
-      map.setView(latlng, zoom); // access the zoom
-  },
-    filterData: function (text, records) {
-      var jsons = fuse.search(text),
-        ret = {}, key;
-
-      for (var i in jsons) {
-        key = jsons[i].properties.NAME;
-          ret[key] = records[key];
-                }
-
-      console.log(jsons, ret);
-          return ret;
-    }
-});
-
-searchControl.on('search:locationfound', function (e) {
-
-  e.layer.setStyle({ fillColor: '#3f0', color: '#0f0' });
-  if (e.layer._popup)
-      e.layer.openPopup();
-
-}).on('search:collapsed', function (e) {
-
-  Boundary.eachLayer(function (layer) {	//restore feature color
-      Boundary.resetStyle(layer);
-  });
-});
-
-map.addControl(searchControl);  //inizialize search control
-
-
-
-
-var osmGeocoderControl = new L.Control.Search({
-  url: 'https://nominatim.openstreetmap.org/search?format=json&q={s}&countrycodes=us',
-  jsonpParam: 'json_callback',
-  propertyName: 'display_name',
-  propertyLoc: ['lat', 'lon'],
-  marker: L.circleMarker([0, 0], { radius: 30 }),
-  autoCollapse: true,
-  autoType: false,
-  minLength: 2,
-  position: 'topright',
-  container: '',
-  moveToLocation: (latlng, name, map) => {
-      USstatesLayerB.eachLayer((l) => {
-          console.log(Date.now());
-          if (turf.booleanPointInPolygon(turf.point([latlng.lng, latlng.lat]), l.feature)) {
-              l.setStyle({ fillColor: "#555" });
-              map.fitBounds(l.getBounds());
-          }
-      });
-  }
-});
-
-map.addControl(osmGeocoderControl);
+//STOP
